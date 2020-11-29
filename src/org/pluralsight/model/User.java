436307f3e23bc6a837a278404430cbdfe9fff759
Aga <paulewicz.aga@gmail.com>
2020-11-29 23:30:47 +0100

    Added getter and setter

--- src/org/pluralsight/model/User.java
+++ src/org/pluralsight/model/User.java
@@ -4,6 +4,7 @@ public class User {
 	
 	private String name;
 	private int aga;
+	private String agnieszka;
 	
 
 	public int getAga() {
@@ -25,6 +26,14 @@ public class User {
 	@Override
 	public String toString() {
 		return "User [name=" + name + "]";
+	}
+
+	public String getAgnieszka() {
+		return agnieszka;
+	}
+
+	public void setAgnieszka(String agnieszka) {
+		this.agnieszka = agnieszka;
 	}
 	
 	
